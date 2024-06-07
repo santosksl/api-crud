37d3da625cda7ed2e431f9be8619aad0a1516cb3
santosksl <162834964@+santosksl@users.noreply.github.com>
2024-06-07 11:15:08 -0300

    feat: modified strings

--- src/useCases/errors/index.ts
+++ src/useCases/errors/index.ts
@@ -1,42 +1,42 @@
 class CategoryAlreadyExistsError extends Error {
     constructor() {
-        super('❌ Category already exists!');
+        super('Category already exists!');
     }
 }
 
 class CategoryDoesNotExistError extends Error {
     constructor() {
-        super('❌ This category does not exist!');
+        super('This category does not exist!');
     }
 }
 
 class SupplierDoesNotExistError extends Error {
     constructor() {
-        super('❌ This supplier does not exist!');
+        super('This supplier does not exist!');
     }
 }
 
 class SupplierAlreadyExistsError extends Error {
     constructor() {
-        super('❌ Supplier already exists!');
+        super('Supplier already exists!');
     }
 }
 
 class ProductAlreadyExistsError extends Error {
     constructor() {
-        super('❌ Product already exists!');
+        super('Product already exists!');
     }
 }
 
 class ProductDoesNotExistError extends Error {
     constructor() {
-        super('❌ This product does not exist!');
+        super('This product does not exist!');
     }
 }
 
 class InvalidCategoryParamsError extends Error {
     constructor() {
-        super('❌ Invalid category parameters!');
+        super('Invalid category parameters!');
     }
 }
 
