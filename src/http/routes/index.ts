846e828cee993b5b52f8166785a9b4951d7de1cc
santosksl <162834964@+santosksl@users.noreply.github.com>
2024-06-07 10:54:37 -0300

    feat: setup cors

--- src/http/routes/index.ts
+++ src/http/routes/index.ts
@@ -1,8 +1,10 @@
+import cors from '@fastify/cors';
 import { FastifyInstance } from 'fastify';
 import { crudRoutes } from './CrudRoutes';
 
 class Routes {
     static RegisterRoutes(routerController: FastifyInstance) {
+        routerController.register(cors, { origin: true });
         routerController.register(crudRoutes);
     }
 }
